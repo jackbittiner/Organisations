18b536df1acd74450e55baceb95083884b31b2aa
Jack Bittiner <jackbittiner@hotmail.com>
2019-06-05 20:35:05 +0100

    :card_file_box: Set-up database connection

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+require("./database/db");
+
 const app = express();
 
 const port = process.env.PORT || 8080;
---
+++ database/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbURI =
+  "mongodb+srv://user:drowssap@financialtechtesttimes-qimzj.mongodb.net/test?retryWrites=true&w=majority";
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10
+};
+
+mongoose.connect(dbURI, options).then(
+  () => {
+    console.log("Database connected!");
+  },
+  err => {
+    console.log("Error connecting to database: ", err);
+  }
+);
+
+require("../models/organisation");
