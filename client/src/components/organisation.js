eadff42642adfde3c6fa98a9a29de834706dedf8
Jack Bittiner <jackbittiner@hotmail.com>
2019-06-09 18:17:05 +0100

    :lipstick: add buttons to organisation card

--- client/src/components/organisation.js
+++ client/src/components/organisation.js
@@ -5,8 +5,14 @@ const Organisation = ({ organisation: { name, yearFounded, revenue } }) => {
   return (
     <OrganisationBox>
       <h1>{name}</h1>
-      <p>{yearFounded}</p>
-      <p>{revenue}</p>
+      <p>Founded: {yearFounded}</p>
+      <p>Revenue: {revenue}</p>
+      <button>
+        <img src={require("./assets/edit.svg")} />
+      </button>
+      <button>
+        <img src={require("./assets/delete.svg")} />
+      </button>
     </OrganisationBox>
   );
 };
